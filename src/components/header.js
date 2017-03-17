0974a705bf73a66f8d4e349e2d5c92f3bf977414
yaniv-l <yanivl@srl.co.il>
2017-03-17 09:04:03 +0200

    bootstrap issues...

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,22 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
 class Header extends Component {
   renderLinks() {
-    if(this.props.authenticated) {
-      return (<li className='nav-item'>
-          <Link className='nav-link' to='/signout'>Sign Out</Link>
-      </li>);
-    }
-    else {
+    if (this.props.authenticated) {
+      // show a link to sign out
+      return <li className="nav-item">
+        <Link className="nav-link" to="/signout">Sign Out</Link>
+      </li>
+    } else {
+      // show a link to sign in or sign up
       return [
-        <li className='nav-item' key={1}>
-          <Link className='nav-link' to='/signin'>Sign In</Link>
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/signin">Sign In</Link>
         </li>,
-        <li className='nav-item'>
-            <Link className='nav-link' to='/signup' key={2}>Sign Up</Link>
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">Sign Up</Link>
         </li>
       ];
     }
@@ -23,10 +24,10 @@ class Header extends Component {
 
   render() {
     return (
-      <nav className='navbar navbar-light'>
-      <Link to='/' className='navbar-brand'>Redux Auth</Link>
-        <ul className='nav navbar-nav'>
-          { this.renderLinks() }
+      <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
+        <ul className="nav navbar-nav">
+          {this.renderLinks()}
         </ul>
       </nav>
     );
