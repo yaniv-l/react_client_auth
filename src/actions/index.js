42d3ef56849520826f71cbc109bcdd40395d87f3
yaniv-l <yanivl@srl.co.il>
2017-03-18 20:58:20 +0200

    finishing signup

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,7 +15,7 @@ export function signinUser({ email, password }) {
     // Submit email and password to the server
     axios.post(`${ROOT_URL}/signin`, { email, password })
       .then(response => {
-        console.log('signed-in');
+        //console.log('signed-in'); // For Debug
         // If request is good:
         // - Update state to indicate user is Authenticated
         dispatch({ type: AUTH_USER });
@@ -34,6 +34,38 @@ export function signinUser({ email, password }) {
         // from a component
         dispatch(authError('Bad login info'));
       });
+  };
+}
+
+export function signupUser({ email, password }) {
+  // Since we are using the redux-thunk middleware, instead of returning an
+  // object, as action creattor typically does, we'll return a function.
+  // this fucntion get one parameter, 'dispatch', which is the reference to
+  // the dispatcher between the actions and reducers. This is enabled using the
+  // redux-thunk middleware.
+  return function (dispatch) {
+    // Submit email and password to the server
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+    .then(response => {
+      //console.log('signed-in'); // For Debug
+      // If request is good:
+      // - Update state to indicate user is Authenticated
+      dispatch({ type: AUTH_USER });
+      // - Save the JWT token
+      console.log(response.data.token);
+      localStorage.setItem('token', response.data.token);
+      // - redirect user the the route '/feature'
+      browserHistory.push('/feature');
+    })
+    .catch(error => {
+      // If request is bad:
+      // - Show an error to the user
+      // Since we're using reduxThunk, we have the ability to call an action
+      // creator directly from here, the current actionCreator, as opposed to
+      // not working with reduxThunk where we can only call an actionCreator
+      // from a component
+      dispatch(authError(error.response.data.error));
+    });
   };
 }
 
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,12 +3,84 @@ import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
+  handleFormSubmit(formProps) {
+    // redux-form will only call handleSubmit once the form is valid, so no need
+    // for adtional validation here
+    // Call the SignUp action creator
+    this.props.signupUser(formProps);
+  }
+
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className='alert alert-danger'>
+          <strong>Aw-Snap!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
+  }
 
   render() {
+    const {
+      handleSubmit,
+      fields: { email, password, passwordConfirmation } } = this.props;
     return (
-      <div>Signup</div>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset className='form-group'>
+          <label>Email:</label>
+          <input className='form-control' {...email} />
+          {email.touched &&
+            email.error &&
+            <div className='error'>{email.error}</div>
+          }
+        </fieldset>
+        <fieldset className='form-group'>
+          <label>Password:</label>
+          <input type='password' className='form-control' {...password} />
+          {password.touched &&
+            password.error &&
+            <div className='error'>{password.error}</div>
+          }
+        </fieldset>
+        <fieldset className='form-group'>
+          <label>Confirm Password:</label>
+          <input type='password' className='form-control' {...passwordConfirmation} />
+          {passwordConfirmation.touched &&
+            passwordConfirmation.error &&
+            <div className='error'>{passwordConfirmation.error}</div>
+          }
+        </fieldset>
+        {this.renderAlert()}
+        <button action='submit' className='btn btn-primary'>Sign Up!</button>
+      </form>
     );
   }
 }
 
-export default Signup;
+function validate(formProps) {
+  const errors = {};
+  if (!formProps.email) {
+    errors.email = 'Please enter an Email';
+  }
+  if (!formProps.password) {
+    errors.password = 'Please enter a Password';
+  }
+  if (!formProps.passwordConfirmation) {
+    errors.passwordConfirmation = 'Please enter Password Confirmation';
+  }
+  if (formProps.password != formProps.passwordConfirmation) {
+    errors.password = 'Password must match'
+  }
+
+  return errors;
+}
+function mapStateToProps(state) {
+  return {
+    errorMessage: state.auth.error
+  };
+}
+export default reduxForm ({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirmation'],
+  validate: validate
+}, mapStateToProps, actions)(Signup);
